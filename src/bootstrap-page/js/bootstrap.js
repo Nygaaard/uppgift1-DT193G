2c463dd9982987e6279499b807741b20db13d437
Nygaaard <nygardandreas@hotmail.com>
2024-11-19 16:39:57 +0100

    Added JS code

--- src/bootstrap-page/js/bootstrap.js
+++ src/bootstrap-page/js/bootstrap.js
@@ -1,6 +1,47 @@
+document.getElementById("addButton").addEventListener("click", function() {
+    var nameEl = document.getElementById("firstname");
+    var ageEl = document.getElementById("age");
+    var messageEl = document.getElementById("message");
+    var genderEls = document.getElementsByName("gender");
+    var errorEl = document.getElementById("errorDiv");
 
+    var genderChecked = false;
+    for (var i = 0; i < genderEls.length; i++) {
+        if (genderEls[i].checked) {
+            genderChecked = true;
+            break;
+        }
+    }
 
+    if (!nameEl.value || !ageEl.value || !messageEl.value || !genderChecked) {
+        errorEl.style.display = "block";
+        return;
+    }
 
-const firstnameEl = document.getElementById("firstname");
-const priceEl = document.getElementById("price");
-const positivesEl = document.getElementById("positives");
+    errorEl.style.display = "none";
+
+    var name = nameEl.value;
+    var age = ageEl.value;
+    var favoriteFramework = messageEl.value;
+
+    var gender = "";
+    for (var j = 0; j < genderEls.length; j++) {
+        if (genderEls[j].checked) {
+            gender = genderEls[j].value;
+            break;
+        }
+    }
+
+    var table = document.getElementById("frameworkTable").getElementsByTagName("tbody")[0];
+    var newRow = table.insertRow();
+    newRow.innerHTML = `
+        <tr>
+            <td>${name}</td>
+            <td>${age}</td>
+            <td>${favoriteFramework}</td>
+            <td>${gender}</td>
+        </tr>
+    `;
+
+    document.getElementById("frameworkForm").reset();
+});
---
+++ src/tailwind-page/js/tailwind.js
@@ -0,0 +1,52 @@
+const tBodyEl = document.getElementById("tBody");
+
+const gridNameEl = document.getElementById("grid-name");
+const gridStateEl = document.getElementById("grid-state");
+const messageEl = document.getElementById("message");
+const genderEls = document.getElementsByName("gender");
+
+const errorEl = document.getElementById("errorDiv");
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    if (!gridNameEl.value || !gridStateEl.value || !messageEl.value || ![...genderEls].some(el => el.checked)) {
+        errorEl.style.display = "block";
+        return;
+    }
+
+    errorEl.style.display = "none";
+
+    const name = gridNameEl.value;
+    const age = gridStateEl.value;
+    const favoriteFramework = messageEl.value;
+
+    let gender = "";
+    for (const genderEl of genderEls) {
+        if (genderEl.checked) {
+            gender = genderEl.value;
+            break;
+        }
+    }
+
+    const newRow = `
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                ${name}
+            </th>
+            <td class="px-6 py-4">
+                ${age}
+            </td>
+            <td class="px-6 py-4">
+                ${favoriteFramework}
+            </td>
+            <td class="px-6 py-4">
+                ${gender}
+            </td>
+        </tr>
+    `;
+
+    tBodyEl.innerHTML += newRow;
+
+    document.querySelector("form").reset();
+});
